204f87bcf766f2fd4ef8769935b1a9f1f9a99b90
Olumide Olaoye <92790819+Olumide09@users.noreply.github.com>
2023-09-29 14:01:52 -0500

    finished assessment

---
+++ profile.js
@@ -0,0 +1,31 @@
+let favColor = document.querySelector('#color')
+
+favColor.addEventListener('click', () => {
+    alert("My favourite color is blue")
+})
+
+// #place
+let favPlace = document.querySelector('#place')
+
+favPlace.addEventListener('click', () => {
+    alert("My Bedroom")
+})
+//#ritual
+
+let favRitual = document.querySelector('#ritual')
+
+favRitual.addEventListener('click', () => {
+    alert("I like to listen to music before taking on tasks")
+})
+
+
+function checkSubmit(event) {
+    const resturants = ["Popeyes", "McDonalds", "Panera Bread", "Chick-fil-a"];
+    const random = Math.floor(Math.random() * resturants);
+
+    alert('random, resturants[random]');
+}
+
+let resSuggestor = document.querySelector('#resturant')
+
+resSuggestor.addEventListener('click', checkSubmit)

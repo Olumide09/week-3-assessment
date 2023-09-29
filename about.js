a8e8f1d2a4ef2aae2e14f2120e6d82914f0b1f95
Olumide Olaoye <92790819+Olumide09@users.noreply.github.com>
2023-09-29 10:57:56 -0500

    first edit on about.js

--- about.js
+++ about.js
@@ -5,10 +5,16 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	alert("form has been submitted successfully");
 }
 
 
 let form = document.querySelector('#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+let rubbduckImg = document.querySelector('#rubduc')
+
+rubbduckImg.addEventListener('mouseover', () => {
+	alert("Give User a Compliment")
+})
